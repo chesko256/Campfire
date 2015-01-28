bc503ff0569ffd6c50403e83ef177022eb2db3cd
Chesko <chesko.tesmod@gmail.com>
2015-01-28 14:06:49 -0600

    autodoc stuff

--- Scripts/Source/doc/githubber.py
+++ Scripts/Source/doc/githubber.py
@@ -12,47 +12,144 @@ lines = f.readlines()
 h = open('./headers/' + str(sys.argv[1]), 'r')
 header = h.readlines()
 
-first_header = True
-toc = []
-toc.append('### Functions\n')
-body = []
-#body.append('## Global Functions\n')
-body.append('\n')
-for line in lines:
-	if line.endswith("**\n"):
-		if "DESCRIPTION" in line:
-			continue
-		elif "RETURN VALUE" in line:
-			line = '**Return Value**\n'
-		else:
-		    line = line.replace('**', '').replace('.\n', '').capitalize()
-		    line = '**' + line + '**\n'
-		body.append(line)
-	elif line.startswith('#'):
-		line = line.replace('### ', '').replace('\n', '')
-		link_line = line.replace('/', '_').replace('\n', '')
-		line_split = line.split('/')
-		line = '#### <a name=\"' + link_line + '\"></a>' + line_split[1] + '()\n'
-		toc_line = link_line.split('_')[1]
-		toc.append('* [' + toc_line + '()](#' + link_line + ')\n')
-		if not first_header:
-		    body.append('***\n')
-		else:
-			first_header = False
-		body.append(line)
-	else:
-		body.append(line)
+property_toc = []
+global_toc = []
+member_toc = []
 
-toc.append('\n')
-toc.append('***')
-toc.append('\n')
+property_body = []
+global_body = []
+member_body = []
+
+first_header = True
+def parse_entry(entry):
+	global first_header
+	#Is this a Global, a Member, or a Property?
+	is_global = False
+	is_member = False
+	is_property = False
+	if any(') global' in line for line in entry):
+	    is_global = True
+	elif any('property' in line for line in entry):
+		is_property = True
+	else:
+		is_member = True
+
+	if is_global:
+		for line in entry:
+			if line.startswith('#'):
+				line = line.replace('### ', '').replace('\n', '')
+				link_line = line.replace('/', '_').replace('\n', '')
+				line_split = line.split('/')
+				line = '#### <a name=\"' + link_line + '\"></a>' + line_split[1] + '()'
+				toc_line = link_line.split('_')[1]
+				if not global_toc:
+					global_toc.append('### Global Functions\n')
+				global_toc.append('* [' + toc_line + '()](#' + link_line + ')\n')
+				if not first_header:
+				    global_body.append('***\n')
+				else:
+					first_header = False
+				if not property_body:
+					property_body.append('\n')
+					property_body.append('## Global Functions\n')
+				global_body.append(line)
+			elif line.endswith("**\n"):
+				if "DESCRIPTION" in line:
+					continue
+				elif "RETURN VALUE" in line:
+					line = '**Return Value**\n'
+				else:
+				    line = line.replace('**', '').replace('.\n', '').capitalize()
+				    line = '**' + line + '**\n'
+				global_body.append(line)
+			else:
+				global_body.append(line)
+	elif is_member:
+		for line in entry:
+			if line.startswith('#'):
+				line = line.replace('### ', '').replace('\n', '')
+				link_line = line.replace('/', '_').replace('\n', '')
+				line_split = line.split('/')
+				line = '#### <a name=\"' + link_line + '\"></a>' + line_split[1] + '()'
+				toc_line = link_line.split('_')[1]
+				if not member_toc:
+					member_toc.append('### Member Functions\n')
+				member_toc.append('* [' + toc_line + '()](#' + link_line + ')\n')
+				if not first_header:
+				    member_body.append('***\n')
+				else:
+					first_header = False
+				if not member_body:
+					property_body.append('## Member Functions\n')
+				member_body.append(line)
+			elif line.endswith("**\n"):
+				if "DESCRIPTION" in line:
+					continue
+				elif "RETURN VALUE" in line:
+					line = '**Return Value**\n'
+				else:
+				    line = line.replace('**', '').replace('.\n', '').capitalize()
+				    line = '**' + line + '**\n'
+				member_body.append(line)
+			else:
+				member_body.append(line)
+	elif is_property:
+		for line in entry:
+			if line.startswith('#'):
+				line = line.replace('### ', '').replace('\n', '')
+				link_line = line.replace('/', '_').replace('\n', '')
+				line_split = line.split('/')
+				line = '#### <a name=\"' + link_line + '\"></a>' + line_split[1]
+				toc_line = link_line.split('_')[1]
+				if not property_toc:
+					property_toc.append('### Properties\n')
+				property_toc.append('* [' + toc_line + '](#' + link_line + ')\n')
+				if not first_header:
+				    property_body.append('***\n')
+				else:
+					first_header = False
+				if not property_body:
+					property_body.append('## Properties\n')
+				property_body.append(line)
+			elif line.endswith("**\n"):
+				if "DESCRIPTION" in line:
+					continue
+				elif "RETURN VALUE" in line:
+					line = '**Return Value**\n'
+				else:
+				    line = line.replace('**', '').replace('.\n', '').capitalize()
+				    line = '**' + line + '**\n'
+				property_body.append(line)
+			else:
+				property_body.append(line)
+
+entry = list()
+entries = list()
+for line in lines:
+	if line.startswith('#'):
+		entries.append(entry)
+		entry = list()
+		entry.append(line)
+	else:
+		entry.append(line)
+else:
+	entries.append(entry)
+
+for e in entries:
+	parse_entry(e)
 
 with open(str(sys.argv[2]), "w") as myfile:
 	for line in header:
 		myfile.write(line)
-
-	for line in toc:
+	for line in property_toc:
 		myfile.write(line)
-
-	for line in body:
+	for line in global_toc:
+		myfile.write(line)
+	for line in member_toc:
+		myfile.write(line)
+	for line in property_body:
+		myfile.write(line)
+	for line in global_body:
+		myfile.write(line)
+	for line in member_body:
 		myfile.write(line)
--- Scripts/Source/doc/makedocs.bat
+++ Scripts/Source/doc/makedocs.bat
@@ -1,3 +1,4 @@
 robodoc.exe
 pandoc.exe "./output/CampUtil_psc.xml" -f docbook -t markdown_github -o "./output/CampUtil_psc.txt" --base-header-level=3
 python githubber.py "CampUtil_psc.txt" "../../../campwiki/CampUtil-API-Reference.md"
+pause
